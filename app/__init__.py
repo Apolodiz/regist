aea8a4436131a769eec9d8a7f15274685635af5b
land-n42@mail.ru <land-n42@mail.ru>
2020-06-03 16:14:00 +0300

    td3

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(name, template_folder=Config.TEMPLATES_DIR)
+app = Flask(__name__, template_folder=Config.TEMPLATES_DIR)
 wsgi_app = app.wsgi_app
 app.config.from_object(Config)
 
