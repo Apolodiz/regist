ac693925dc3e570b0ba70d0f2930d1580bbad40e
land-n42@mail.ru <land-n42@mail.ru>
2020-06-03 15:57:49 +0300

    config_template_dir

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(name, template_folder=Config.TEMPLATES_DIR)
 wsgi_app = app.wsgi_app
 app.config.from_object(Config)
 
@@ -13,3 +14,4 @@ migrate = Migrate(app, db)
 login=LoginManager(app)
 login.login_view = 'login'
 from app import routes, models
+init.py
